ff94896e020ac460fe15b4aeba9015d7760f47ef
unknown <ariel.breboneria@gmail.com>
2021-11-08 19:08:40 +0800

    Add initial bundle.js

---
+++ public/bundle.js
@@ -0,0 +1,24 @@
+let today = new Date();
+
+let hours = today.getHours();
+if (hours < 12){
+    var ampm = "AM";
+}   else {
+    var ampm = "PM";
+    hours = hours - 12
+}
+if (hours == 0){
+    hours = hours + 12
+}
+hours = addZero(hours)
+let minutes = addZero(today.getMinutes());
+let seconds = addZero(today.getSeconds());
+
+function addZero(num){
+    return num < 10 ? `0${num}`:num;
+}
+
+let time = `${hours}:${minutes}:${seconds} ${ampm}`
+document.getElementById('time').innerHTML=time;
+
+exports.time = time;
